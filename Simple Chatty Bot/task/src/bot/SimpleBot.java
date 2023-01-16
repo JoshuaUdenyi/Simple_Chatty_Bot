6efa9815f0c55b253503a8ce34a995e742964a7e
JoshuaUdenyi <udenyijoshua95@gmail.com>
2023-01-16 13:21:44 +0100

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,25 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int rem1 = scan.nextInt();
+        int rem2 = scan.nextInt();
+        int rem3 = scan.nextInt();
+        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
+    }
+}
